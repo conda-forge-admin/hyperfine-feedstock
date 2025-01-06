b15bacb50a812dae92f7680e501d9a30902196e8
Brandon Maier <brandon.maier@gmail.com>
2025-01-06 08:00:33 -0600

    install hyperfine man page

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,3 +11,7 @@ cargo-bundle-licenses \
 
 # build statically linked binary with Rust
 cargo install --no-track --locked --root "$PREFIX" --path .
+
+# Install the man page
+mkdir -p "${PREFIX}/share/man/man1"
+install -pm 0644 doc/hyperfine.1 "${PREFIX}/share/man/man1/hyperfine.1"
